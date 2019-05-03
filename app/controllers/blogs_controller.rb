45806e55fa6a29d0541528592ee59e969123a3c0
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-05-04 01:07:47 +0900

    コントローラーを編集 プライベート以下にストロングパラメーターを定義

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,24 @@
 class BlogsController < ApplicationController
   def index
+    @blog = Blog.all
   end
 
   def new
+    @blog = Blog.new
   end
 
+  def create
+   Blog.create(blog_params)
+   redirect_to new_blog_path
+  end
+
+
+
+
+
+  private
+
+def blog_params
+  params.require(:blog).permit(:content)
+end
 end
