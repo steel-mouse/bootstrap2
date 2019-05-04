56cf59acef1a126ab76fbeb2f2cf73dc2f425309
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-05-04 21:06:45 +0900

    ブログ編集機能を追加。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,7 +21,12 @@ class BlogsController < ApplicationController
   end
 
   def update
-
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
   end
 
 
