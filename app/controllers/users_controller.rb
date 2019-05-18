cf3c2a61155ffe7fd7511d5188a0fd69ca53bac8
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-05-18 16:49:35 +0900

    routes にcreateを追加 create アクションを編集 エラー文が出るようにnew htmlを編集。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功した場合の処理
+    else
+      render 'new'
+    end
+  end
+
+
+
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
       get :top
     end
   end
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
 end
