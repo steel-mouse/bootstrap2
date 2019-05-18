10a8411c0abfb550945d129f9115c862e2a79a9b
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-05-19 00:11:14 +0900

    メソッドを定義。

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
+
+  # ログイン中のユーザーを取得するメソッド
   def current_user
+    # 左が真の場合はそのまま、偽の場合は右の処理が実行される。
   @current_user ||= User.find_by(id: session[:user_id])
 end
+
+# ユーザーがログインしていればtrue、その他ならfalseを返すメソッド
+  def logged_in?
+    current_user.present?
+  end
 end
