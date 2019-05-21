1b1ba1d60c13be2c3519d73eb7c49cb7c258c69b
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-05-22 02:27:48 +0900

    デバック用のコマンドを記載。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -45,6 +45,7 @@ class BlogsController < ApplicationController
 
   def confirm
     @blog = Blog.new(blog_params)
+    # binding.pry
     @blog.user_id = current_user.id
     render :new if @blog.invalid?
   end
